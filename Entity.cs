57b59c9201532799c0b4d320c8c3f1732fead5c8
Ana Carolina Arellano <anacarolinaarellano@gmail.com>
2021-02-11 01:12:51 +0000

    Entity class changed

--- Entity.cs
+++ Entity.cs
@@ -1,7 +1,8 @@
 using System;
 class Entity
 {
+  char identifier
   int health;
-  int hit; //Percentage
-
+  int hit;      //Percentage chance to be hit
+  int attack;   //Damage they deal when they hit
 }
